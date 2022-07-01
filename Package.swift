4d7373e737425f09365300d6b4795b6bbdabb935
Armend Hasani <armend.hasani926@gmail.com>
2022-07-01 16:26:07 +0200

    Update demo app and add new framework versions

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
           name: "FuturaeKit",
-          url: "https://artifactory.futurae.com/artifactory/futurae-ios/FuturaeKit-v1.5.0.xcframework.zip",
-          checksum: "a9c2105ea84b8e376b03a3038a4f933f15d762c423aa145423e9ab3895a76079"
+          url: "https://artifactory.futurae.com/artifactory/futurae-ios/FuturaeKit-v1.5.1.xcframework.zip",
+          checksum: "f74222728cf827a3a9fca691251a224ea9ab473902ea0a494bf6ef44afe06302"
         )
     ]
 )
