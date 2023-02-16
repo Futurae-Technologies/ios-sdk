87b3fb40e72e5b784bacc11742f9e28b24da8984
Armend Hasani <armend@futurae.com>
2023-02-16 18:44:46 +0100

    Add missing demo files

---
+++ FuturaeDemo/Helpers/FuturaeDemo-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  FuturaeDemo-Bridging-Header.h
+//  FuturaeDemo
+//
+//  Created by Ruben Dudek on 17/01/2023.
+//  Copyright © 2023 Futurae. All rights reserved.
+//
+
+#import "HistoryItem.h"
+
+#ifndef FuturaeDemo_Bridging_Header_h
+#define FuturaeDemo_Bridging_Header_h
+
+
+#endif /* FuturaeDemo_Bridging_Header_h */
