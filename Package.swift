51fb6f080b10615e422dc2f85c2c04f2b9236b7e
Armend Hasani <armend@futurae.com>
2024-06-07 16:42:56 +0200

    Update framework to version 2.4.0

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
           name: "FuturaeKit",
-          url: "https://github.com/Futurae-Technologies/ios-sdk/releases/download/v2.3.14/FuturaeKit-v2.3.14.xcframework.zip",
-          checksum: "f7f7f3bc6392253cba9e610fe62c31906b6b66a13c0bc1989f43031902af79de"
+          url: "https://github.com/Futurae-Technologies/ios-sdk/releases/download/v2.4.0/FuturaeKit-v2.4.0.xcframework.zip",
+          checksum: "30e520b8c9e720619548f194957105a0f1714873cb8f902c29aae5469ee67953"
         )
     ]
 )
