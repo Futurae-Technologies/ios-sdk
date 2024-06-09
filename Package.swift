e7674215c82b53029f94d409dd139a62a249c628
Armend Hasani <armend@futurae.com>
2024-06-09 11:31:16 +0200

    Update Package.swift file

--- Package.swift
+++ Package.swift
@@ -1,19 +1,30 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "FuturaeKit",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v12)
     ],
     products: [
-        .library(name: "FuturaeKit", targets: ["FuturaeKit"])
+        .library(name: "FuturaeKit", targets: ["FuturaeKit", "FuturaeKitUmbrella"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", .upToNextMajor(from: "0.14.1")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.6.0")),
     ],
     targets: [
         .binaryTarget(
-          name: "FuturaeKit",
-          url: "https://github.com/Futurae-Technologies/ios-sdk/releases/download/v2.4.0/FuturaeKit-v2.4.0.xcframework.zip",
-          checksum: "0d005c307c4d31d2119aa3dadea3df4465c369ba0ca8d1b0b29bff927d7ab3db"
+            name: "FuturaeKit",
+            url: "https://github.com/Futurae-Technologies/ios-sdk/releases/download/v3.2.0/FuturaeKit-v3.2.0.xcframework.zip",
+            checksum: "b93d94315de8c3396304f3a24d3a5734237a1a2a1508fbb9cf62bb87cfb4e654"
+        ),
+        .target(
+            name: "FuturaeKitUmbrella",
+            dependencies: [
+                .product(name: "RxSwift", package: "RxSwift"),
+                .product(name: "SQLite", package: "SQLite.swift"),
+            ]
         )
     ]
 )
