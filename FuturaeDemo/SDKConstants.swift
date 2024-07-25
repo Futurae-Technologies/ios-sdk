9170fc0e5226baa0c2c2117a67f62f5c90faa89a
Armend Hasani <armend@futurae.com>
2024-07-25 15:40:49 +0200

    Update framework to version 2.4.1

--- FuturaeDemo/SDKConstants.swift
+++ FuturaeDemo/SDKConstants.swift
@@ -8,20 +8,19 @@
 
 import Foundation
 
-class SDKConstants: NSObject {
-    static let SDKID = "sdk_id"
-    static let SDKKEY = "sdk_key"
-    static let SDKURL = "https://api.futurae.com"
+public class SDKConstants: NSObject {
+    @objc public static let SDKID = ""
+    @objc public static let SDKKEY = ""
+    @objc public static let SDKURL = "https://api-test.futurae.com"
     
-    static let APP_GROUP = "group.futurae"
-    static let APP_ID = "T82Z6CGNMT.com.futurae.FuturaeDemo"
-    static let KEYCHAIN_ACCESS_GROUP = "group.futurae"
+    @objc public static let APP_GROUP = "group.futuraedemo"
+    @objc public static let KEYCHAIN_ACCESS_GROUP = "group.futuraedemo"
     
-    static let USER_ID_KEY = "user_id"
-    static let EXTRA_INFO_ENC_KEY = "extra_info_enc"
-    static let DEVICE_TOKEN_KEY = "ftr_device_token"
-    static let ADAPTIVE_ENABLED_KEY = "adaptive_enabled"
-    static let KEY_CONFIG = "key_config"
-    static let APP_GROUP_ENABLED = "app_group_enabled"
-
+    @objc public static let USER_ID_KEY = "user_id"
+    @objc public static let EXTRA_INFO_ENC_KEY = "extra_info_enc"
+    @objc public static let DEVICE_TOKEN_KEY = "ftr_device_token"
+    @objc public static let ADAPTIVE_ENABLED_KEY = "adaptive_enabled"
+    @objc public static let KEY_CONFIG = "key_config"
+    @objc public static let APP_GROUP_ENABLED = "app_group_enabled"
+    
 }
--- Package.swift
+++ Package.swift
@@ -10,14 +10,14 @@ let package = Package(
         .library(name: "FuturaeKit", targets: ["FuturaeKit", "FuturaeKitUmbrella"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/stephencelis/SQLite.swift.git", .upToNextMajor(from: "0.14.1")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.6.0")),
+        .package(url: "https://github.com/Futurae-Technologies/ios-sdk/releases/download/v2.4.1/FuturaeKit-v2.4.1.xcframework.zip", .upToNextMajor(from: "0.14.1")),
+        .package(url: "https://github.com/Futurae-Technologies/ios-sdk/releases/download/v2.4.1/FuturaeKit-v2.4.1.xcframework.zip", .upToNextMajor(from: "6.6.0")),
     ],
     targets: [
         .binaryTarget(
             name: "FuturaeKit",
-            url: "https://github.com/Futurae-Technologies/ios-sdk/releases/download/v3.2.1/FuturaeKit-v3.2.1.xcframework.zip",
-            checksum: "23a61f10e634bfcafa17790f57640fbe08d5ff63288d96396ca762a052961615"
+            url: "https://github.com/Futurae-Technologies/ios-sdk/releases/download/v2.4.1/FuturaeKit-v2.4.1.xcframework.zip",
+            checksum: "434565f7bd03f0f3a5494df34383c348661dfb64d32d0fac71881f36abe5f612"
         ),
         .target(
             name: "FuturaeKitUmbrella",
