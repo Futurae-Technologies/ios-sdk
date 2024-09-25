382d5e8310a6c70e76dd02b3215cf2450ab06a6e
Armend Hasani <armend@futurae.com>
2024-09-25 13:46:57 +0200

    Fix Package.swift file

--- Package.swift
+++ Package.swift
@@ -7,24 +7,13 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        .library(name: "FuturaeKit", targets: ["FuturaeKit", "FuturaeKitUmbrella"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/Futurae-Technologies/ios-sdk/releases/download/v2.5.0/FuturaeKit-v2.5.0.xcframework.zip", .upToNextMajor(from: "0.15.3")),
-        .package(url: "https://github.com/Futurae-Technologies/ios-sdk/releases/download/v2.5.0/FuturaeKit-v2.5.0.xcframework.zip", .upToNextMajor(from: "6.7.1")),
+        .library(name: "FuturaeKit", targets: ["FuturaeKit"]),
     ],
     targets: [
         .binaryTarget(
             name: "FuturaeKit",
             url: "https://github.com/Futurae-Technologies/ios-sdk/releases/download/v3.4.0/FuturaeKit-v3.4.0.xcframework.zip",
             checksum: "baedbc690dae46f657a62eb2eb90d91da501cbf44d791c921e9dc3d7a00f35cf"
-        ),
-        .target(
-            name: "FuturaeKitUmbrella",
-            dependencies: [
-                .product(name: "RxSwift", package: "RxSwift"),
-                .product(name: "SQLite", package: "SQLite.swift"),
-            ]
         )
     ]
 )
