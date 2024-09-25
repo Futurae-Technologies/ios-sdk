82034090ced93c0c26d27a1bf1b391b3fde8209e
Armend Hasani <armend@futurae.com>
2024-09-25 15:26:33 +0200

    Update Package.swift file

--- Package.swift
+++ Package.swift
@@ -7,24 +7,13 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        .library(name: "FuturaeKit", targets: ["FuturaeKit", "FuturaeKitUmbrella"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/stephencelis/SQLite.swift.git", .upToNextMajor(from: "0.14.1")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.6.0")),
+        .library(name: "FuturaeKit", targets: ["FuturaeKit"]),
     ],
     targets: [
         .binaryTarget(
             name: "FuturaeKit",
             url: "https://github.com/Futurae-Technologies/ios-sdk/releases/download/v2.4.1/FuturaeKit-v2.4.1.xcframework.zip",
             checksum: "434565f7bd03f0f3a5494df34383c348661dfb64d32d0fac71881f36abe5f612"
-        ),
-        .target(
-            name: "FuturaeKitUmbrella",
-            dependencies: [
-                .product(name: "RxSwift", package: "RxSwift"),
-                .product(name: "SQLite", package: "SQLite.swift"),
-            ]
         )
     ]
 )
