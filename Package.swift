d567e85bd72e5b4064184f80db71fdbcae773dfc
Armend Hasani <armend@futurae.com>
2025-02-14 17:26:06 +0100

    Updated package swift to point to new version.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .binaryTarget(
             name: "FuturaeKit",
             url: "https://github.com/Futurae-Technologies/ios-sdk/releases/download/v3.6.4/FuturaeKit-v3.6.4.xcframework.zip",
-            checksum: "6007ed3b6cc048987a52b1394ab790a603cedd1cadd7695b1f575c886a5189aa"
+            checksum: "aa91605f7ad3ca3090169ad7737b1afb064077bc87691f4cfad7331089709a45"
         )
     ]
 )
