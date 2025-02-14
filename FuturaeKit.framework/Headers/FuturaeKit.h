7b4dfc7c300c251f32d295066ca6810b0b4fd6f4
Armend Hasani <armend@futurae.com>
2025-02-14 17:26:05 +0100

    Added frameworks for carthage.

---
+++ FuturaeKit.framework/Headers/FuturaeKit.h
@@ -0,0 +1,21 @@
+//
+//  FuturaeKit.h
+//  FuturaeKit
+//
+//  Created by Dimitris Togias on 27/12/2017.
+//  Copyright (C) 2018 Futurae Technologies AG - All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FuturaeKit.
+FOUNDATION_EXPORT double FuturaeKitVersionNumber;
+
+//! Project version string for FuturaeKit.
+FOUNDATION_EXPORT const unsigned char FuturaeKitVersionString[];
+
+// In this header, you should import all thwe public headers of your framework using statements like #import <FuturaeKit/PublicHeader.h>
+#import <FuturaeKit/FTRAdaptiveClientDelegate.h>
+#import <FuturaeKit/FTRAdaptiveSDKDelegate.h>
+#import <FuturaeKit/FTRAdaptiveClient.h>
+
